183fdc04acd8af126ab4f4650391967ff1136456
Taessan Kang <taekang@c1r3s7.42seoul.kr>
2020-10-03 19:58:18 +0900

    Libft commit

---
+++ Libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taekang <taekang@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 22:12:31 by taekang           #+#    #+#             */
+/*   Updated: 2020/10/01 19:11:31 by taekang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+ #define LIBFT_H
+
+#include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
+
+#endif
