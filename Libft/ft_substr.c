f8fb1fafa0d112d5defdb81d327d8459e2aebe83
Taessan Kang <pawer02023@gmail.com>
2020-10-19 20:12:33 +0900

    source commit

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: taekang <taekang@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 12:04:00 by taekang           #+#    #+#             */
-/*   Updated: 2020/10/11 13:46:10 by taekang          ###   ########.fr       */
+/*   Updated: 2020/10/19 20:10:34 by taekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*dst;
 	char	*src;
 
+	if (!s)
+		return (0); 
 	src = (char *)s;
 	if (!(dst = (char *)malloc(sizeof(char) * (len + 1))))
 	{
 		return (0);
 	}
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
 	ft_strlcpy(dst, (s + start), len + 1);
 	return (dst);
 }
