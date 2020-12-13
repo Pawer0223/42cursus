83799c97cbb9459d73fb08219ff23dced3a002df
NAME <gbgo77@naver.com>
2020-12-13 17:49:05 +0900

    working

--- Printf/sources/ft_set_com.c
+++ Printf/sources/ft_set_com.c
@@ -10,3 +10,49 @@ void			fill_is_zero(t_input *input, int idx)
 	else
 		input->str[idx] = '0';
 }
+
+int		get_len_ll(long long n)
+{
+	int len;
+	int nmg;
+
+	len = n <= 0 ? 1 : 0;
+	if (n < 0)
+		n *= -1;
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_lltoa(long long n)
+{
+	char		*str;
+	long long	num;
+	int			len;
+	int			start;
+
+
+
+	num = n;
+	len = get_len(num);
+	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
+		return (0);
+	str[len--] = '\0';
+	start = 0;
+	if (num < 0)
+	{
+		num *= -1;
+		str[start] = '-';
+		start++;
+	}
+	while (len >= start)
+	{
+		str[len] = '0' + (num % 10);
+		num /= 10;
+		len--;
+	}
+	return (str);
+}
--- Printf/sources/ft_set_f.c
+++ Printf/sources/ft_set_f.c
@@ -1,58 +1,6 @@
 #include "printf.h"
 #include "libft.h"
 
-void			fill_str_left(t_input* input, long long n, int idx)
-{
-	int nmg;
-
-	if (n == 0)
-		input->str[idx--] = '0';
-	while (idx >= 0 && n != 0)
-	{
-		nmg = n % 10;
-		if (nmg < 0)
-			nmg *= -1;
-		input->str[idx--] = '0' + nmg;
-		n /= 10;
-	}
-	if (input->sign && idx >= 0)
-		input->str[idx--] = input->sign;
-	while (idx >= 0)
-		input->str[idx--] = '0';
-}
-
-void			fill_str_f(t_input *input, double n, int idx)
-{
-	int			num;
-	int			i;
-	long long	ll;
-	double		d;
-	int			len_r;
-	int			pre_len;
-
-	ll = n;
-	d = n - ll;
-	pre_len = g_info->precision_len;
-
-	if (pre_len != -1)
-		len_r = pre_len;
-	else
-		len_r = 6;
-	i = idx - len_r;
-	if (len_r != 0)
-		input->str[i--] = '.';
-	fill_str_left(input, ll, i);
-	i += 2;
-	while (i < input->len)
-	{
-		num = d * 10;
-		d = (d * 10) - num;
-		if (num < 0)
-			num *= -1;
-		input->str[i++] = num + '0';
-	}
-}
-
 void			set_f_sign(t_input *input, double n)
 {
 	if (n < 0)
@@ -65,28 +13,17 @@ void			set_f_sign(t_input *input, double n)
 
 int				setlen_f(t_input *input, double n)
 {
-	int			len;
-	int			pre_len;
-	long long	ll;
+	long long ll;
+	double d;
 
 	ll = n;
-	pre_len = g_info->precision_len;
-	len = (ll == 0) ? 1 : 0;
-	while (ll != 0)
+	d = n - ll;
+	if (g_info->precision_len == 0)
 	{
-		ll /= 10;
-		len++;
+		if ((int)d * 10 >= 5 && (ll % 2 != 0))
+			ll += 1;
 	}
-	if (pre_len != -1)
-		len += pre_len;
-	else
-		len += 7;
-	if (pre_len != 0)
-		len++;
-	if (input->sign)
-		len++;
-	input->len = len;
-	return (len);
+
 }
 
 int				set_f_input(double n)
@@ -98,6 +35,7 @@ int				set_f_input(double n)
 	if (!(input = (t_input *)(malloc(sizeof(t_input)))))
 		return (0);
 	g_info->input = input;
+	set_left_f(n);
 	set_f_sign(input, n);
 	size = setlen_f(input, n);
 	if (!(str = (char*)malloc(sizeof(char) * (size + 1))))
