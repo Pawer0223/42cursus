e9eee9ee81c1a5503e99f8dd681d42797bfa3aed
Taesan <gbgo77@naver.com>
2020-12-27 17:40:13 +0900

    compile sh modify

--- Printf/sources/test/exec.sh
+++ Printf/sources/test/exec.sh
@@ -1,2 +1,3 @@
 # gcc main.c ../*.c ../libft/*.c ../../headers/*.h -lm 
-gcc -g /home/taesan/42cursus/Printf/sources/test/main.c /home/taesan/42cursus/Printf/sources/*.c /home/taesan/42cursus/Printf/sources/libft/*.c /home/taesan/42cursus/Printf/headers/*.h -lm
+# gcc -g /home/taesan/42cursus/Printf/sources/test/main.c /home/taesan/42cursus/Printf/sources/*.c /home/taesan/42cursus/Printf/sources/libft/*.c /home/taesan/42cursus/Printf/headers/*.h -lm
+gcc /home/taesan/42cursus/Printf/sources/test/main.c -L /home/taesan/42cursus/Printf/ -lftprintf -lm
