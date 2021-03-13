99847b91dc3685adc4ddaaae3fa47790aaeb1665
taesan <pawer02023@gmail.com>
2021-03-14 03:40:30 +0900

    cub3d norm check

--- Cub3d/cub3d.c
+++ Cub3d/cub3d.c
@@ -6,21 +6,27 @@
 /*   By: taekang <taekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 00:15:40 by taekang           #+#    #+#             */
-/*   Updated: 2021/03/12 19:36:42 by taekang          ###   ########.fr       */
+/*   Updated: 2021/03/14 03:40:10 by taekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/* up is test code */
 void	clear_buf(t_cub3d *info)
 {
-	for (int i = 0; i < info->win_height; i++)
+	int i;
+	int j;
+
+	i = 0;
+	while (i < info->win_height)
 	{
-		for (int j = 0; j < info->win_width; j++)
+		j = 0;
+		while (j < info->win_width)
 		{
 			info->buf[i][j] = 0;
+			j++;
 		}
+		i++;
 	}
 }
 
@@ -28,10 +34,10 @@ void	raycasting(t_cub3d *info)
 {
 	int		x;
 	t_ray	ray;
-	t_draw 	draw;
+	t_draw	draw;
 
 	x = 0;
-	while(x < info->win_width)
+	while (x < info->win_width)
 	{
 		shoot_ray(&ray, info, x);
 		fill_data(&ray, &draw, info, x);
@@ -42,18 +48,25 @@ void	raycasting(t_cub3d *info)
 
 void	draw(t_cub3d *info)
 {
-	for (int y = 0; y < info->win_height; y++)
+	int y;
+	int x;
+
+	y = 0;
+	while (y < info->win_height)
 	{
-		for (int x = 0; x < info->win_width; x++)
+		x = 0;
+		while (x < info->win_width)
 		{
 			info->img.data[y * info->win_width + x] = info->buf[y][x];
+			x++;
 		}
+		y++;
 	}
 	mlx_put_image_to_window(info->mlx, info->win, info->img.img, 0, 0);
 	clear_buf(info);
 }
 
-int	main_loop(t_cub3d *info)
+int		main_loop(t_cub3d *info)
 {
 	raycasting(info);
 	draw(info);
