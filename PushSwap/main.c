38a3ddbbf2686536d131c6a05f1544a9ff59cdb5
taesan <pawer02023@gmail.com>
2021-05-05 13:46:14 +0900

    commit

---
+++ PushSwap/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "../Libft/libft.h"
+
+int		main(void)
+{
+	int len = ft_strlen("len");
+	printf("len : %d\n", len);
+	printf("hi");
+}
