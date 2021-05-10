498d51a272c6a98ea2943adcc6f66f2f10f6cec5
taesan <pawer02023@gmail.com>
2021-05-10 22:45:10 +0900

    working

--- PushSwap/error.c
+++ PushSwap/error.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.h                                            :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/23 14:12:48 by taekang           #+#    #+#             */
-/*   Updated: 2021/05/10 18:13:43 by taesan           ###   ########.fr       */
+/*   Created: 2021/05/10 18:13:58 by taesan            #+#    #+#             */
+/*   Updated: 2021/05/10 18:43:43 by taesan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ERROR_H
-# define ERROR_H
+# include "push_swap.h"
 
-# define ERROR "Error"
+void	delete_content(void *content)
+{
+	free(content);
+}
 
-#endif
+int		error_occur(t_list **list)
+{
+	ft_lstclear(list, delete_content);
+	ft_putendl_fd(ERROR, 1);
+	return (0);
+}
--- PushSwap/push_swap.h
+++ PushSwap/push_swap.h
@@ -6,18 +6,22 @@
 /*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 19:43:00 by taesan            #+#    #+#             */
-/*   Updated: 2021/05/10 17:36:25 by taesan           ###   ########.fr       */
+/*   Updated: 2021/05/10 19:31:48 by taesan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# define MAX "2147483647"
+# define MIN "2147483648"
+
 # include "libft/libft.h"
 # include "error.h"
 # include <stdio.h> // remove !
 
-
+int		error_occur(t_list **list);
+void	delete_content(void *content);
 
 
 # endif
