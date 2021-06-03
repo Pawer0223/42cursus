897a55ed1213d5342463c020cd7a9053cc3e6a56
taesan <pawer02023@gmail.com>
2021-06-04 04:07:02 +0900

    test code modify

--- PushSwap/test.sh
+++ PushSwap/test.sh
@@ -1,6 +1,14 @@
+export YELLOW='\033[1;33m'
+export NO_COLOR='\033[0m'
+
 gcc -o ./my_test/random ./my_test/random_gene.c;
 export ARG=$(./my_test/random $1);
-echo "$1 개 데이터 테스트";
+echo "${YELLOW}--------------------test data--------------------";
+echo ${NO_COLOR}$ARG;
+echo "${YELLOW}-----------------Checker Result------------------${NO_COLOR}";
 ./push_swap "$ARG" | ./checker "$ARG";
-./push_swap "$ARG" | wc -l;
-echo $ARG;
+echo "${YELLOW}----------------Instruction Cnt------------------${NO_COLOR}";
+./push_swap "$ARG" | wc -l | tr -d " ";
+# echo "--------------------leak test start--------------------";
+# leaks -atExit -- ./push_swap "$ARG";
+# echo "--------------------leak test end--------------------";
