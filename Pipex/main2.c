f521797aeb7169af9f5e39611b2d1f1ac6fd79c1
taesan <pawer02023@gmail.com>
2021-06-28 13:51:18 +0900

    file save success

--- Pipex/main2.c
+++ Pipex/main2.c
@@ -6,7 +6,7 @@
 /*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 21:19:34 by taesan            #+#    #+#             */
-/*   Updated: 2021/06/27 21:43:01 by taesan           ###   ########.fr       */
+/*   Updated: 2021/06/28 13:50:49 by taesan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,31 @@ void	exec_command(char *param[], int pipefd[2], int flags)
 	perror("execv");
 }
 
+void	exec_command_test(char *param[], int pipefd[2], int flags, int result_fd)
+{
+	pid_t	cpid;
+
+	cpid = fork();
+	
+	if (cpid > 0)
+		return;
+	else if (cpid < 0)
+		perror("fork");
+	char *const envp[] = {NULL};
+	if (flags & STDIN_PIPE)
+		if (dup2(pipefd[0], STDIN_FILENO) < 0)
+			perror("dup2");
+	if (flags & STDOUT_PIPE) {
+		printf("here fd : %d\n", result_fd);
+		if (dup2(result_fd, STDOUT_FILENO) < 0)
+			perror("dup2");
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	execve(param[0], param, envp);
+	perror("execv");
+}
+
 int		input_pipe_fill(const char *file, int fds[2])
 {
 	int		fd;
@@ -132,32 +157,38 @@ int		main(int argc, const char *argv[], char *envp[])
 		pipe(fds);          // populates fds with two file descriptors
 		pipe(fds_2);
 
+		// 결과 파일.
+		int result_fd;
+		if ((result_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR)) == -1)
+			return (error_occur());
 		// 첫번째 파이프에 데이터 쓰기.
 		if (!(input_pipe_fill(argv[1], fds)))
 			return (0);
 		close(fds[1]);
 
 		char **cmd_info;
-		// printf("fds[0] : %d, fds[1] : %d, fds_2[0] : %d, fds_2[1] : %d\n", fds[0], fds[1], fds_2[0], fds_2[1]);
-
-		// 두번 째 명령어를 입력 대기 상태로.
-		cmd_info = temp(argv[3], path);
-
-		// 그 전에 출력 디스크립터를 파일로 변경하기.
-		int fd;
-		if ((fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, S_IRUSR)) == -1)
-			return (error_occur());
-		exec_command(cmd_info, fds_2, STDIN_PIPE);
-		close(fds_2[0]);
+		printf("fds[0] : %d, fds[1] : %d, fds_2[0] : %d, fds_2[1] : %d\n", fds[0], fds[1], fds_2[0], fds_2[1]);
 
 		// 파이프를 연결하기.
+		// 첫번째 명령어를 pipe1에서 입력받아, pipe2로 출력하도록한다. 즉, pipe2를 채워준다.
 		int connect_fds[2] = {fds[0], fds_2[1]};
-
-		split_free(cmd_info);
 		cmd_info = temp(argv[2], path);
 		exec_command(cmd_info, connect_fds, STDIN_PIPE | STDOUT_PIPE);
 		close(fds[0]);
 		close(fds_2[1]);
+
+		// 마지막 명령어는 입력을 pipe2에서 받아 처리한다.
+		split_free(cmd_info);
+		cmd_info = temp(argv[3], path);
+		// exec_command(cmd_info, fds_2, STDIN_PIPE);
+		exec_command_test(cmd_info, fds_2, STDIN_PIPE | STDOUT_PIPE, result_fd);
+		
+		// 메모리 해제
+		close(fds_2[0]);
+		close(result_fd);
+		split_free(cmd_info);
+
+
 
 		// 부모에서 자식 프로세스가 끝나기를 대기하는 코드.
 		while (wait(&status) > 0);
