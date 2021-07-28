74969bbbf1ddabb9ebc27f638bd1f50c12ed0371
taesan <pawer02023@gmail.com>
2021-07-28 21:02:19 +0900

    commit

--- Minishell/srcs/check.c
+++ Minishell/srcs/check.c
@@ -6,11 +6,23 @@
 /*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/25 23:50:42 by taesan            #+#    #+#             */
-/*   Updated: 2021/07/26 15:41:43 by taesan           ###   ########.fr       */
+/*   Updated: 2021/07/28 20:55:33 by taesan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+char	*create_new_cmd(char *cmd, int len)
+{
+	char	*new_cmd;
+
+	new_cmd = (char *)malloc(sizeof(len + 1));
+	if (!new_cmd)
+		return (0);
+	new_cmd[0] = '/';
+	ft_strlcpy(new_cmd + 1, cmd, len + 1);
+	return (new_cmd);
+}
 
 int	check_builtin(char *cmd)
 {
@@ -31,35 +43,31 @@ int	check_builtin(char *cmd)
 	return (NOT_BUILTIN);
 }
 
-/*
-	pipe 존재하는지
-	큰 따옴표나 작은 따옴표 존재하는지 && 홀 수 인경우 에러
-	큰 따옴표, 작은 따옴표 동시에 존재하면, 먼저 어떤걸로 적용할지.
-*/
-void	check_input(char *input, t_info *info)
+char	*check_command(char **paths, char *cmd, int len)
 {
-	info->single_q = 0;
-	info->double_q = 0;
-	info->pipe_cnt = 0;
-	info->start_q = 0;
-	while (*input)
+	char	*full_path;
+	char	*new_cmd;
+	int		i;
+
+	full_path = 0;
+	new_cmd = create_new_cmd(cmd, len);
+	if (new_cmd)
 	{
-		if (*input == SINGLE_Q)
-			info->single_q++;
-		else if (*input == DOUBLE_Q)
-			info->double_q++;
-		else if (*input == PIPE)
-			info->pipe_cnt++;
-		if (info->single_q == 1 && info->double_q == 0)
-			info->start_q = SINGLE_Q;
-		else if (info->single_q == 0 && info->double_q == 1)
-			info->start_q = DOUBLE_Q;
-		input += 1;
+		i = 0;
+		while (paths[i])
+		{
+			/*
+				access변경해주어야 함.
+			*/
+			full_path = ft_strjoin(paths[i], new_cmd);
+			if (!full_path || access(full_path, X_OK) == 0)
+				break ;
+			free(full_path);
+			full_path = 0;
+			i++;
+		}
+		free(new_cmd);
 	}
-	/*
-		wait closing quote후에 , 최초 명령어가 수행되어야 한다.
-		반환형 변경 or wait closing 후에 처리되는 로직을 심어야 함.
-	*/
-	if (info->single_q % 2 != 0 || info->double_q % 2 != 0)
-		wait_closing_quote(info->start_q);
+	return (full_path);
 }
+
