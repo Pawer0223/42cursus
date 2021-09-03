cd370d5ef1e73c24be1fafe51145bf9e2d7eba06
taesan <pawer02023@gmail.com>
2021-09-03 15:44:30 +0900

    norm

--- Minishell/srcs/status_macro.c
+++ Minishell/srcs/status_macro.c
@@ -6,12 +6,12 @@
 /*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 15:19:49 by taesan            #+#    #+#             */
-/*   Updated: 2021/09/03 15:33:36 by taesan           ###   ########.fr       */
+/*   Updated: 2021/09/03 15:44:24 by taesan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-	
+
 int	exit_status(int status)
 {
 	return ((status >> 8) & 0x000000ff);
